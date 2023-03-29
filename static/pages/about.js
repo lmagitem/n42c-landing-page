0ec71bf48ff52b588e2329c8471897f62c9ffd81
Luc Toupense <luc.toupense@itesoft.com>
2023-03-29 23:34:19 +0200

    Basic pages done

--- static/pages/about.js
+++ static/pages/about.js
@@ -1,4 +1,33 @@
 export const aboutContent = {
   content: `<h1 id="page-title" class="align-center padded-bottom">About</h1>
-  <div id="page-wrapper"></div>`,
+  <div id="page-wrapper">
+  <div dir="auto" class="line-height-1-5" align="center">
+  <p dir="auto">Find me on:</p>
+  <p dir="auto">
+    <a href="/lmagitem/lmagitem/blob/main/www.linkedin.com/in/luc-toupense"
+      ><img
+        src="https://camo.githubusercontent.com/b57d8b58b19cc50aa7da8599eb8540db90b6f0cc480bbec0b0823bd8f4692c8a/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4c696e6b6564496e2d4c75635f546f7570656e73652d626c75653f6c6f676f3d6c696e6b6564696e267374796c653d666c61742d737175617265"
+        alt="LinkedIn"
+        data-canonical-src="https://img.shields.io/badge/LinkedIn-Luc_Toupense-blue?logo=linkedin&amp;style=flat-square"
+        style="max-width: 100%"
+    /></a>
+    <a href="https://github.com/lmagitem" rel="nofollow"
+      ><img
+      src="https://camo.githubusercontent.com/ade25f855d020a3b0113060f538018ecd2c4cbcf4a4bd9b63550b71fdc30eb04/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4769744875622d6c6d61676974656d2d6c69676874677265793f6c6f676f3d676974687562267374796c653d666c61742d737175617265"
+      alt="GitHub"
+      data-canonical-src="https://img.shields.io/badge/GitHub-lmagitem-lightgrey?logo=github&amp;style=flat-square"
+      style="max-width: 100%"
+  /></a>
+    <a
+      href="https://www.codingame.com/profile/04fd6995b4434e5be2ceb85e2d7c86f78043874"
+      rel="nofollow"
+      ><img
+        src="https://camo.githubusercontent.com/5046a953d150d7055c24642da01dc7fd923099e8d551b91b5d6f05cb6c580c09/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f436f64696e47616d652d6c6d61676974656d2d79656c6c6f773f6c6f676f3d636f64696e67616d65267374796c653d666c61742d737175617265"
+        alt="CodinGame"
+        data-canonical-src="https://img.shields.io/badge/CodinGame-lmagitem-yellow?logo=codingame&amp;style=flat-square"
+        style="max-width: 100%"
+    /></a>
+  </p>
+</div>
+  </div>`,
 };
--- static/pages/projects.js
+++ static/pages/projects.js
@@ -1,4 +1,131 @@
 export const projectsContent = {
   content: `<h1 id="page-title" class="align-center padded-bottom">Projects</h1>
-  <div id="page-wrapper"></div>`,
+  <div id="page-wrapper">
+  <div dir="auto" class="line-height-1-5">
+  <ul dir="auto">
+    <li>
+      <p dir="auto">
+        I'm currently learning
+        <a href="https://www.rust-lang.org/" rel="nofollow"
+          ><img
+            src="https://camo.githubusercontent.com/9f82085145e2912f2284ae1b88b5c53453cb8dcb5f0ad47edef1101e2b5994f3/68747470733a2f2f736869656c64732e696f2f62616467652f2d527573742d6f72616e67653f6c6f676f3d72757374267374796c653d706c6173746963"
+            alt="Rust"
+            data-canonical-src="https://shields.io/badge/-Rust-orange?logo=rust&amp;style=plastic"
+            style="max-width: 100%"
+        /></a>
+      </p>
+    </li>
+    <li>
+      <p dir="auto">I'm working on:</p>
+      <ul dir="auto">
+        <li>
+          <a href="https://github.com/lmagitem/planet-generator"
+            >Planet Generator</a
+          >: a
+          <a href="https://www.rust-lang.org/" rel="nofollow"
+            ><img
+              src="https://camo.githubusercontent.com/9f82085145e2912f2284ae1b88b5c53453cb8dcb5f0ad47edef1101e2b5994f3/68747470733a2f2f736869656c64732e696f2f62616467652f2d527573742d6f72616e67653f6c6f676f3d72757374267374796c653d706c6173746963"
+              alt="Rust"
+              data-canonical-src="https://shields.io/badge/-Rust-orange?logo=rust&amp;style=plastic"
+              style="max-width: 100%"
+          /></a>
+          library that aims at generating galaxies, sectors, solar systems,
+          planets with maps and their inhabitants alongside tons of narrative
+          elements and ideas
+        </li>
+        <li>
+          <a href="https://github.com/lmagitem/galactic-scanner"
+            >Galactic Scanner</a
+          >: an
+          <a href="https://actix.rs/" rel="nofollow"
+            ><img
+              src="https://camo.githubusercontent.com/78441713dc02bc9680df5aa37401ea33dc83ad4349b043acd064207508837f51/68747470733a2f2f736869656c64732e696f2f62616467652f2d41637469782d626c756576696f6c65743f6c6f676f3d72757374267374796c653d706c6173746963"
+              alt="Actix"
+              data-canonical-src="https://shields.io/badge/-Actix-blueviolet?logo=rust&amp;style=plastic"
+              style="max-width: 100%"
+          /></a>
+          server that allows
+          <a href="https://github.com/lmagitem/galactic-explorer"
+            >Galactic Explorer</a
+          >
+          to play with
+          <a href="https://github.com/lmagitem/planet-generator"
+            >Planet Generator</a
+          >
+        </li>
+        <li>
+          <a href="https://github.com/lmagitem/galactic-explorer"
+            >Galactic Explorer</a
+          >: a
+          <a href="https://github.com/facebook/react/"
+            ><img
+              src="https://camo.githubusercontent.com/2df81a63a4a8da8896ab469a3733dd5242e74b8e2c91598fa482a8d1832f52ef/68747470733a2f2f736869656c64732e696f2f62616467652f2d52656163742d626c75653f6c6f676f3d7265616374267374796c653d706c6173746963"
+              alt="React"
+              data-canonical-src="https://shields.io/badge/-React-blue?logo=react&amp;style=plastic"
+              style="max-width: 100%"
+          /></a>
+          app to play with
+          <a href="https://github.com/lmagitem/planet-generator"
+            >Planet Generator</a
+          >
+        </li>
+        <li>
+          <a href="https://github.com/lmagitem/n42c-landing-page"
+            >N42C landing page</a
+          >: an
+          <a href="https://actix.rs/" rel="nofollow"
+            ><img
+              src="https://camo.githubusercontent.com/78441713dc02bc9680df5aa37401ea33dc83ad4349b043acd064207508837f51/68747470733a2f2f736869656c64732e696f2f62616467652f2d41637469782d626c756576696f6c65743f6c6f676f3d72757374267374796c653d706c6173746963"
+              alt="Actix"
+              data-canonical-src="https://shields.io/badge/-Actix-blueviolet?logo=rust&amp;style=plastic"
+              style="max-width: 100%"
+          /></a>
+          server that serves a
+          <a href="/lmagitem/lmagitem/blob/main"
+            ><img
+              src="https://camo.githubusercontent.com/f2e3a6e7b09ad660e71153fd0c41c1ae2c8504ceb5791064c806fc3bf89870ee/68747470733a2f2f736869656c64732e696f2f62616467652f2d4a6176617363726970742d79656c6c6f773f6c6f676f3d6a617661736372697074267374796c653d706c6173746963"
+              alt="vanilla Javascript"
+              data-canonical-src="https://shields.io/badge/-Javascript-yellow?logo=javascript&amp;style=plastic"
+              style="max-width: 100%"
+          /></a>
+          website with basic routing, lazy loading, internationalization and
+          analytics to replace my old Wordpress blog and serve as a simple
+          portfolio
+        </li>
+      </ul>
+    </li>
+    <li>
+      <p dir="auto">I maintain:</p>
+      <ul dir="auto">
+        <li>
+          <a href="https://github.com/lmagitem/seeded-dice-roller"
+            >Seeded Dice Roller</a
+          >: a
+          <a href="https://www.rust-lang.org/" rel="nofollow"
+            ><img
+              src="https://camo.githubusercontent.com/9f82085145e2912f2284ae1b88b5c53453cb8dcb5f0ad47edef1101e2b5994f3/68747470733a2f2f736869656c64732e696f2f62616467652f2d527573742d6f72616e67653f6c6f676f3d72757374267374796c653d706c6173746963"
+              alt="Rust"
+              data-canonical-src="https://shields.io/badge/-Rust-orange?logo=rust&amp;style=plastic"
+              style="max-width: 100%"
+          /></a>
+          library that rolls dice and generates pseudo-random results in a
+          deterministic way
+        </li>
+        <li>
+          <a href="https://github.com/lmagitem/goldencross">Golden Cross</a>: an
+          <a href="https://angular.io/" rel="nofollow"
+            ><img
+              src="https://camo.githubusercontent.com/833703a33cc12e16b37c29d271ef869799ce760ce64a0937682f2f53d49a48da/68747470733a2f2f736869656c64732e696f2f62616467652f2d416e67756c61722d7265643f6c6f676f3d616e67756c6172267374796c653d706c6173746963"
+              alt="Angular"
+              data-canonical-src="https://shields.io/badge/-Angular-red?logo=angular&amp;style=plastic"
+              style="max-width: 100%"
+          /></a>
+          app allowing to plan entries on the stock market by looking at golden
+          crosses on moving averages
+        </li>
+      </ul>
+    </li>
+  </ul>
+</div>
+  </div>`,
 };
