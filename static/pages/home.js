18bb0fab47e6728cc45d6c723835997af7249b38
Luc Toupense <luc.toupense@itesoft.com>
2023-03-29 23:45:29 +0200

    Todo list

--- static/pages/home.js
+++ static/pages/home.js
@@ -2,6 +2,33 @@ export const homeContent = {
   content: `<h1 id="page-title" class="align-center padded-bottom">Welcome to N42C!</h1>
   <div id="page-wrapper">
   <p class="align-center padded-bottom">Please, feel comfortable and use the buttons up-there to see what wonders this website is hiding...</p>
+  <ul dir="auto"> Yet todo:
+  <li>
+    <p dir="auto">
+      Translate every page
+    </p>
+  </li>
+  <li>
+    <p dir="auto">
+      Set up the domains properly
+    </p>
+  </li>
+  <li>
+    <p dir="auto">
+      Update the Nginx
+    </p>
+  </li>
+  <li>
+    <p dir="auto">
+      Bundle the project and upload it on my server
+    </p>
+  </li>
+  <li>
+    <p dir="auto">
+      Resume work on planet-generator and galactic-explorer
+    </p>
+  </li>
+  </ul>
   </div>`,
 };
 
