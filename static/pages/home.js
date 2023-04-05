d6e9478ed3d66e08e0d55a39e4783afb9f781ad3
Luc Magitem <31952766+lmagitem@users.noreply.github.com>
2023-04-05 23:00:54 +0200

    Minor changes

--- static/pages/home.js
+++ static/pages/home.js
@@ -1,31 +1,9 @@
 export const homeContent = {
-  contentEn: `<h1 id="page-title" class="align-center padded-bottom">Welcome to N42C!</h1>
+    contentEn: `<h1 id="page-title" class="align-center padded-bottom">Welcome to N42C!</h1>
   <div id="page-wrapper">
   <p class="align-center padded-bottom">Please, feel comfortable and use the buttons up-there to see what wonders this website is hiding...</p>
-  <ul dir="auto"> Yet todo:
-  <li>
-    <p dir="auto">
-      Set up the domains properly
-    </p>
-  </li>
-  <li>
-    <p dir="auto">
-      Update the Nginx
-    </p>
-  </li>
-  <li>
-    <p dir="auto">
-      Bundle the project and upload it on my server
-    </p>
-  </li>
-  <li>
-    <p dir="auto">
-      Resume work on planet-generator and galactic-explorer
-    </p>
-  </li>
-  </ul>
   </div>`,
-  contentFr: `<h1 id="page-title" class="align-center padded-bottom">Bienvenue chez N42C !</h1>
+    contentFr: `<h1 id="page-title" class="align-center padded-bottom">Bienvenue chez N42C !</h1>
   <div id="page-wrapper">
     <p class="align-center padded-bottom">
       Mettez-vous à l'aise et utilisez les boutons du dessus pour découvrir les merveilles que vous réserve ce site web...
@@ -34,9 +12,9 @@ export const homeContent = {
 };
 
 export const buttonTranslations = {
-  aboutFr: "<h3>À propos</h3>",
-  aboutEn: "<h3>About</h3>",
-  blog: "<h3>Blog</h3>",
-  projectsFr: "<h3>Projets</h3>",
-  projectsEn: "<h3>Projects</h3>",
+    aboutFr: "<h3>À propos</h3>",
+    aboutEn: "<h3>About</h3>",
+    blog: "<h3>Blog</h3>",
+    projectsFr: "<h3>Projets</h3>",
+    projectsEn: "<h3>Projects</h3>",
 };
