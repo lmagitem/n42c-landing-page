420d8ffc61a8a33b6bdd5760a7b0b436af652b31
lmagitem <lukemagitem@gmail.com>
2023-04-13 08:33:15 +0200

    WIP portfolio

---
+++ static/pages/portfolio.js
@@ -0,0 +1,106 @@
+export const portfolioContent = {
+  contentEn: `<h1 id="page-title" class="align-center padded-bottom">404 - Not Found</h1>
+  <div id="page-wrapper">
+  <p><img class="aligncenter wp-image-677 size-full" src="https://n42c.files.wordpress.com/2018/07/head.png" alt="" width="644" height="162" /><a href="https://drive.google.com/file/d/1Zw9RCxwabHkHhhKyey_ED5_EOfLGsGly"><img class="aligncenter wp-image-679 size-full" src="https://n42c.files.wordpress.com/2018/07/cv1.png" alt="" width="644" height="81" /></a><a name="sommaire"></a><img class="aligncenter wp-image-678 size-full" src="https://n42c.files.wordpress.com/2018/07/presommaire.png" alt="" width="644" height="69" /><a href="#programmation"><img class="aligncenter wp-image-680 size-full" style="margin-top: 0; margin-bottom: 0;" src="https://n42c.files.wordpress.com/2018/07/sommaire1.png" alt="" width="644" height="57" /></a><a href="#jeuvideo"><img class="aligncenter wp-image-681 size-full" src="https://n42c.files.wordpress.com/2018/07/sommaire2.png" alt="" width="644" height="57" /></a><a href="#graphisme"><img class="aligncenter wp-image-682 size-full" src="https://n42c.files.wordpress.com/2018/07/sommaire3.png" alt="" width="644" height="57" /></a><a href="#audiovisuel"><img class="aligncenter wp-image-683 size-full" src="https://n42c.files.wordpress.com/2018/07/sommaire4.png" alt="" width="644" height="57" /></a><a href="#illustration"><img class="aligncenter wp-image-684 size-full" src="https://n42c.files.wordpress.com/2018/07/sommaire5.png" alt="" width="644" height="57" /></a><br /><a href="#sommaire" name="programmation"><img class="aligncenter wp-image-689 size-full" src="https://n42c.files.wordpress.com/2018/07/programmation.png" alt="" width="644" height="365" /></a><img class="aligncenter wp-image-690 size-full" src="https://n42c.files.wordpress.com/2018/07/disclaimer.png" alt="" width="644" height="181" /><a href="#sommaire" name="jeuvideo"><img class="aligncenter wp-image-688 size-full" src="https://n42c.files.wordpress.com/2018/07/jeuvideo.png" alt="" width="644" height="365" /></a></p>
+<h5>PurpleBlueRed - Participation à la <em>Ludum Dare 41<br /></em></h5>
+<p style="text-align: justify;">En équipe. Participation à la création en 48h d'un mélange entre jeu de course et jeu de puzzle (<a href="https://ldjam.com/events/ludum-dare/41/purplebluered-pbr">version rendue disponible ici</a>).</p>
+<p><img class="aligncenter wp-image-697 size-large" src="https://n42c.files.wordpress.com/2018/07/ludum18.png?w=900" alt="" width="900" height="693" /></p>
+<p><img class="aligncenter wp-image-586 size-full" src="https://n42c.files.wordpress.com/2017/01/barre1.png" alt="" width="644" height="227" /></p>
+<h5>Vegan Runner - Participation à la <a href="http://www.labfab.fr/game-jam-festival-k-barre/">Game Jam du Festival K-Barré</a></h5>
+<p style="text-align: justify;">En équipe. Participation à la création en 48h d'un jeu d'arcade en vue de dessus destiné à être présenté lors du festival K-Barré.</p>
+<p><img class="aligncenter wp-image-700 size-large" src="https://n42c.files.wordpress.com/2018/07/vegan.png?w=900" alt="" width="900" height="524" /></p>
+<p><img class="aligncenter wp-image-692 size-full" src="https://n42c.files.wordpress.com/2018/07/j2017.png" alt="" width="644" height="227" /></p>
+<h5>Hamsterpower - Participation à la <em>Ludum Dare 39<br /></em></h5>
+<p style="text-align: justify;">Conception en 48h d'un mélange entre un casse-briques et un jeu de réflexes (<a href="https://ldjam.com/events/ludum-dare/39/hamsterpower">version rendue disponible ici</a>).</p>
+<p><img class="aligncenter wp-image-648 size-medium" src="https://n42c.files.wordpress.com/2017/11/pic.png?w=300" alt="" width="300" height="300" /></p>
+<p> </p>
+<h5>Siboot - Jeu vidéo de narration interactive, par Chris Crawford</h5>
+<p style="text-align: justify;">Création d'un langage visuel pour l'interface utilisateur du jeu, réalisation d'un <a href="http://siboot.org/">site internet présentant le projet</a> et montage d'une vidéo de présentation pour une campagne de financement participatif.</p>
+<p>[gallery type="rectangular" ids="566,567"]</p>
+<p>https://www.youtube.com/watch?v=2UimYOKfisM</p>
+<p><img class="aligncenter wp-image-594 size-full" src="https://n42c.files.wordpress.com/2017/01/j2015.png" width="644" height="227" /></p>
+<h5>Cherchons ! - Proposition pour le concours d'entrée de l'ENJMIN</h5>
+<p style="text-align: justify;">Création d'un concept de jeu, conception de mécaniques et présentation du tout via un <em><a href="https://www.dropbox.com/s/dcrodzuibtruyw6/LucToupense_GD.pdf?dl=0">game design document</a> </em>complet.</p>
+<p>[gallery columns="2" size="medium" ids="556,557,558,559"]</p>
+<p><img class="aligncenter wp-image-586 size-full" src="https://n42c.files.wordpress.com/2017/01/barre1.png" width="644" height="227" /></p>
+<h5>Le Roman de Goupil - Participation à la <em>Ludum Dare 32<br /></em></h5>
+<p style="text-align: justify;">Conception en 48h d'un petit jeu de fiction interactive (<a href="http://ludumdare.com/compo/ludum-dare-32/?action=preview&amp;uid=25147">version rendue disponible ici</a>).</p>
+<p>[gallery type="rectangular" ids="301,302,304"]</p>
+<p><img class="aligncenter wp-image-586 size-full" src="https://n42c.files.wordpress.com/2017/01/barre1.png" width="644" height="227" /></p>
+<h5>Teen Age - Participation à la <em>Global Game Jam 2015<br /></em></h5>
+<p style="text-align: justify;">Conception en 48h de la simulation d'un environnement dans lequel des agents "intelligents" forment ou quittent des groupes en fonction de leurs affinités et de la pression de leurs pairs (<a href="http://globalgamejam.org/2015/games/teen-age">version rendue disponible ici</a>).</p>
+<p>[gallery type="rectangular" ids="438,277,276"]</p>
+<p><img class="aligncenter wp-image-595 size-full" src="https://n42c.files.wordpress.com/2017/01/j2014.png" width="644" height="227" /></p>
+<h5>Power Disco Crush Color Saga - Participation à la <em>Global Game Jam 2014<br /></em></h5>
+<p style="text-align: justify;">En équipe. Création d'un concept et conception de ressources graphiques en 48h (<a href="http://globalgamejam.org/2014/games/power-disco-crush-color-saga-candy-edition">version rendue disponible ici</a>).</p>
+<p>[gallery type="rectangular" ids="570,571,572"]</p>
+<p>https://www.youtube.com/watch?v=54C2315q_Oc</p>
+<p><img class="aligncenter wp-image-596 size-full" src="https://n42c.files.wordpress.com/2017/01/j2013.png" width="644" height="227" /></p>
+<h5>Projets divers</h5>
+<p style="text-align: justify;">Entraînement à la conception de mécaniques de jeu, production de ressources graphiques.</p>
+<p>[gallery type="rectangular" columns="2" ids="282,281,258,74,82,41,543,544"]</p>
+<p><a href="https://n42c.files.wordpress.com/2018/10/graphisme.png" name="graphisme"><img class="aligncenter wp-image-709 size-full" src="https://n42c.files.wordpress.com/2018/10/graphisme.png" alt="" width="644" height="365" /></a></p>
+<h5>39, 40, 41, 42, 43 et 44èmes Festivals du Cinéma américain de Deauville</h5>
+<p style="text-align: justify;">Gestion du site internet, de sa photothèque et de sa vidéothèque pendant le déroulement du festival.</p>
+<p>[gallery type="rectangular" columns="2" ids="615,616"]</p>
+<p><img class="aligncenter wp-image-586 size-full" src="https://n42c.files.wordpress.com/2017/01/barre1.png" width="644" height="227" /></p>
+<h5>68, 69, 70, 71, 72 et 73èmes catalogues du Congrès de la Fédération nationale du Cinéma français</h5>
+<p style="text-align: justify;">Mise en page des numéros annuels d'après une maquette prédéfinie.</p>
+<p>[gallery type="square" ids="618,620,619,621,622"]</p>
+<p><img class="aligncenter wp-image-589 size-full" src="https://n42c.files.wordpress.com/2017/01/g20141.png" width="644" height="227" /></p>
+<h5>Linard Avocats - <em>Newsletter</em> et encarts publicitaires pour le site pagesjaunes.fr</h5>
+<p style="text-align: justify;">Conception et exécution.</p>
+<p>[gallery columns="2" size="medium" type="rectangular" ids="507,508,509,510"]</p>
+<p><img class="aligncenter wp-image-588 size-full" src="https://n42c.files.wordpress.com/2017/01/g2013.png" width="644" height="227" /></p>
+<h5 class="entity-control-field">MagGuffin - Magazine étudiant</h5>
+<p style="text-align: justify;">Conception de la maquette et mise en page de deux numéros.</p>
+<p>[gallery type="slideshow" columns="6" ids="520,521,522,523,524,525,526,527,528,529,530,531,532,533,534,535,536,537,538,539,540,541"]</p>
+<p><img class="aligncenter wp-image-590 size-full" src="https://n42c.files.wordpress.com/2017/01/g2012.png" width="644" height="227" /></p>
+<h5>Seven Friday - Site internet</h5>
+<p style="text-align: justify;">Conception de maquette.</p>
+<p>[gallery columns="2" size="medium" type="square" ids="488,485,486,487"]</p>
+<p><img class="aligncenter wp-image-591 size-full" src="https://n42c.files.wordpress.com/2017/01/g2009.png" width="644" height="227" /></p>
+<h5>Initiative Informatique - Logotype et site internet</h5>
+<p style="text-align: justify;">Recherches d'identité visuelle et conception de maquette. Réalisé en stage.<br /><a href="https://n42c.files.wordpress.com/2016/12/cvbook_luctoupense-4.png"><img class="aligncenter wp-image-489 size-large" style="margin-top: 20px; margin-bottom: 30px;" src="https://n42c.files.wordpress.com/2016/12/cvbook_luctoupense-4.png?w=900" alt="cvbook_luctoupense-4" width="900" height="642" /></a><img class="aligncenter wp-image-586 size-full" src="https://n42c.files.wordpress.com/2017/01/barre1.png" width="644" height="227" /></p>
+<h5>Centre International de Deauville - Programme des spectacles</h5>
+<p style="text-align: justify;">Exécution d'après une charte donnée. Réalisé en stage.</p>
+<h5><a href="https://n42c.files.wordpress.com/2016/12/spectacles.png"><img class="aligncenter wp-image-504 size-large" style="margin-bottom: 30px;" src="https://n42c.files.wordpress.com/2016/12/spectacles.png?w=900" alt="spectacles" width="900" height="801" /></a><img class="aligncenter wp-image-586 size-full" src="https://n42c.files.wordpress.com/2017/01/barre1.png" width="644" height="227" /></h5>
+<h5>La Closerie Deauville - Dépliant</h5>
+<p style="text-align: justify;">Conception et exécution d'après une charte graphique. Réalisé en stage.</p>
+<p>[gallery type="rectangular" ids="502,503"]</p>
+<p><img class="aligncenter wp-image-592 size-full" src="https://n42c.files.wordpress.com/2017/01/g2008.png" width="644" height="227" /></p>
+<h5>Sine me - Site internet personnel</h5>
+<p style="text-align: justify;">Conception de maquette.<br /><img class="aligncenter wp-image-607 size-full" style="margin-top: 20px; margin-bottom: 30px;" src="https://n42c.files.wordpress.com/2017/01/sinemel.png" width="1600" height="864" /></p>
+<p><a href="#sommaire" name="audiovisuel"><img class="aligncenter wp-image-597 size-full" src="https://n42c.files.wordpress.com/2017/01/audiovisuel.png" width="644" height="365" /></a></p>
+<h5>Refuge - Court-métrage personnel</h5>
+<p style="text-align: justify;">Écriture et réalisation.</p>
+<p>http://www.youtube.com/watch?v=_0G1lnvUxfk<br /><img class="aligncenter wp-image-599 size-full" src="https://n42c.files.wordpress.com/2017/01/a2014.png" width="644" height="227" /></p>
+<h5>Instants de jeu - Court-métrage documentaire</h5>
+<p style="text-align: justify;">Conception, production, réalisation, participation à la prise de vue et montage.</p>
+<p>https://www.youtube.com/watch?v=yrJYjICcwhQ<br /><img class="aligncenter wp-image-586 size-full" src="https://n42c.files.wordpress.com/2017/01/barre1.png" width="644" height="227" /></p>
+<h5>Caval'Océane - Vidéo promotionnelle</h5>
+<p style="text-align: justify;">Assistanat à la production et renfort cadre.</p>
+<p>http://www.youtube.com/watch?v=CYse-BYZlJ0<br /><img class="aligncenter wp-image-600 size-full" src="https://n42c.files.wordpress.com/2017/01/a2013.png" width="644" height="227" /></p>
+<h5 class="entity-control-field"><span class="field-text" dir="ltr">Hôtels Mercure Bretagne - Vidéos promotionnelles<br /></span></h5>
+<p style="text-align: justify;">Assistanat à la production sur une dizaine de vidéos de ce type.</p>
+<p>https://youtu.be/sUtXG9hZC7A<br /><a href="#sommaire" name="illustration"><img class="aligncenter wp-image-601 size-full" src="https://n42c.files.wordpress.com/2017/01/illustration.png" width="644" height="365" /></a></p>
+<h5>Loin des yeux, loin du cœur - Participation au concours de bande dessinée du CROUS</h5>
+<p style="text-align: justify;">Réalisation d'une bande dessinée sur le thème "Ailleurs".</p>
+<p>[gallery type="square" ids="380,381,382,383,384,385"]</p>
+<p><img class="aligncenter wp-image-602 size-full" src="https://n42c.files.wordpress.com/2017/01/i2013.png" width="644" height="227" /></p>
+<h5>Masques - Participation au concours de bande dessinée du CROUS</h5>
+<p style="text-align: justify;">Réalisation d'une bande dessinée sur le thème "Masques".</p>
+<p>[gallery type="rectangular" ids="613,614"]</p>
+<p><img class="aligncenter wp-image-603 size-full" src="https://n42c.files.wordpress.com/2017/01/i2012.png" width="644" height="227" /></p>
+<h5>Super Student World - Participation au concours de bande dessinée du CROUS</h5>
+<p>Réalisation d'une bande dessinée sur le thème "Vie étudiante".</p>
+<p>[gallery type="square" ids="609,610,611,612"]</p>
+<p><img class="aligncenter wp-image-604 size-full" src="https://n42c.files.wordpress.com/2017/01/iapprox.png" width="644" height="227" /></p>
+<h5>Illustrations diverses</h5>
+<p>[gallery type="rectangular" ids="451,490,513,494,491,492,493,499"]</p>
+<p><img class="aligncenter wp-image-605 size-full" src="https://n42c.files.wordpress.com/2017/01/fin.png" width="644" height="227" /></p>
+  </div>`,
+  contentFr: `<h1 id="page-title" class="align-center padded-bottom">404 - Non trouvé</h1>
+  <div id="page-wrapper">
+  </div>`,
+};
